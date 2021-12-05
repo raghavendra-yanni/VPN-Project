01c4a6b0b0914b22b083ba4cfb12860d22894d25
raghavendra-yanni <raghavendrayanni@gmail.com>
2021-12-05 16:46:43 +0530

    Repo made

--- src/communication/threads/ForwardServerClientThread.java
+++ src/communication/threads/ForwardServerClientThread.java
@@ -3,7 +3,13 @@ package communication.threads; /**
  * connects two sockets and starts the TCP forwarding between given client
  * and its assigned server. After the forwarding is failed and the two threads
  * are stopped, closes the sockets.
- *
+ * <p>
+ * <p>
+ * Modifications for IK2206:
+ * - Server pool removed
+ * - Two variants - client connects to listening socket or client is already connected
+ * <p>
+ * Peter Sjodin, KTH
  */
 
 /**
@@ -23,8 +29,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class ForwardServerClientThread extends Thread
-{
+public class ForwardServerClientThread extends Thread {
     private ForwardClient mForwardClient = null;
     private Socket mClientSocket = null;
     private Socket mServerSocket = null;
@@ -40,20 +45,18 @@ public class ForwardServerClientThread extends Thread
      * A client socket should be connected and passed to this constructor.
      * A server socket is created later by encrypt() method.
      */
-    public ForwardServerClientThread(Socket aClientSocket, String serverhost, int serverport)
-    {
+    public ForwardServerClientThread(Socket aClientSocket, String serverhost, int serverport) {
         mClientSocket = aClientSocket;
         mServerPort = serverport;
         mServerHost = serverhost;
     }
- 
+
     /**
      * Creates a client thread for handling clients of NakovForwardServer.
      * Wait for client to connect on client listening socket.
      * A server socket is created later by encrypt() method.
      */
-    public ForwardServerClientThread(ServerSocket listensocket, String serverhost, int serverport) throws IOException
-    {
+    public ForwardServerClientThread(ServerSocket listensocket, String serverhost, int serverport) throws IOException {
         mListenSocket = listensocket;
         //mServerHost =  listensocket.getInetAddress().getHostAddress();
         mServerPort = serverport;
@@ -69,74 +72,77 @@ public class ForwardServerClientThread extends Thread
      * Starts two threads for forwarding : "client in <--> dest server out" and
      * "dest server in <--> client out", waits until one of these threads stop
      * due to read/write failure or connection closure. Closes opened connections.
-     * 
+     *
      * If there is a listen socket, first wait for incoming connection
      * on the listen socket.
      */
-    public void run()
-    {
+    public void run() {
         try {
- 
+
             // Wait for incoming connection on listen socket, if there is one 
-           if (mListenSocket != null) {
-               mClientSocket = mListenSocket.accept();
-               mClientHostPort = mClientSocket.getInetAddress().getHostAddress() + ":" + mClientSocket.getPort();
-               Logger.log("Accepted from  " + mServerPort + " <--> " + mClientHostPort + "  started.");
-               
-           }
-           else {
-               mClientHostPort = mClientSocket.getInetAddress().getHostAddress() + ":" + mClientSocket.getPort();
-           }
+            if (mListenSocket != null) {
+                mClientSocket = mListenSocket.accept();
+                mClientHostPort = mClientSocket.getInetAddress().getHostAddress() + ":" + mClientSocket.getPort();
+                Logger.log("Accepted from  " + mServerPort + " <--> " + mClientHostPort + "  started.");
 
-           try {
-               mServerSocket = new Socket(mServerHost, mServerPort);
-           } catch (Exception e) {
-               System.out.println("Connection failed to " + mServerHost + ":" + mServerPort);
-               e.printStackTrace(); 
-               // Prints what exception has been thrown 
-               System.out.println(e); 
-           }
+            } else {
+                mClientHostPort = mClientSocket.getInetAddress().getHostAddress() + ":" + mClientSocket.getPort();
+            }
 
-           // Obtain input and output streams of server and client
-           InputStream clientIn = mClientSocket.getInputStream();
-           OutputStream clientOut = mClientSocket.getOutputStream();
-           InputStream serverIn = mServerSocket.getInputStream();
-           OutputStream serverOut = mServerSocket.getOutputStream();
+            try {
+                mServerSocket = new Socket(mServerHost, mServerPort);
+            } catch (Exception e) {
+                System.out.println("Connection failed to " + mServerHost + ":" + mServerPort);
+                e.printStackTrace();
+                // Prints what exception has been thrown
+                System.out.println(e);
+            }
 
-           mServerHostPort = mServerHost + ":" + mServerPort;
-           Logger.log("TCP Forwarding  " + mClientHostPort + " <--> " + mServerHostPort + "  started.");
- 
-           // Start forwarding of socket data between server and client
-           ForwardThread clientForward = new ForwardThread(this, clientIn, serverOut);
-           ForwardThread serverForward = new ForwardThread(this, serverIn, clientOut);
-           mBothConnectionsAreAlive = true;
-           clientForward.start();
-           serverForward.start();
- 
+            // Obtain input and output streams of server and client
+            InputStream clientIn = mClientSocket.getInputStream();
+            OutputStream clientOut = mClientSocket.getOutputStream();
+            InputStream serverIn = mServerSocket.getInputStream();
+            OutputStream serverOut = mServerSocket.getOutputStream();
+
+            mServerHostPort = mServerHost + ":" + mServerPort;
+            Logger.log("TCP Forwarding  " + mClientHostPort + " <--> " + mServerHostPort + "  started.");
+
+            // Start forwarding of socket data between server and client
+            ForwardThread clientForward = new ForwardThread(this, clientIn, serverOut);
+            ForwardThread serverForward = new ForwardThread(this, serverIn, clientOut);
+            mBothConnectionsAreAlive = true;
+            clientForward.start();
+            serverForward.start();
+
         } catch (IOException ioe) {
-           ioe.printStackTrace();
+            ioe.printStackTrace();
         }
     }
- 
+
     /**
      * connectionBroken() method is called by forwarding child communication.asdf.threads to notify
      * this thread (their parent thread) that one of the connections (server or client)
      * is broken (a read/write failure occured). This method disconnects both server
      * and client sockets causing both communication.asdf.threads to stop forwarding.
      */
-    public synchronized void connectionBroken()
-    {
+    public synchronized void connectionBroken() {
         if (mBothConnectionsAreAlive) {
-           // One of the connections is broken. Close the other connection and stop forwarding
-           // Closing these socket connections will close their input/output streams
-           // and that way will stop the communication.asdf.threads that read from these streams
-           try { mServerSocket.close(); } catch (IOException e) {}
-           try { mClientSocket.close(); } catch (IOException e) {}
- 
-           mBothConnectionsAreAlive = false;
- 
-           Logger.log("TCP Forwarding  " + mClientHostPort + " <--> " + mServerHostPort + "  stopped.");
+            // One of the connections is broken. Close the other connection and stop forwarding
+            // Closing these socket connections will close their input/output streams
+            // and that way will stop the communication.asdf.threads that read from these streams
+            try {
+                mServerSocket.close();
+            } catch (IOException e) {
+            }
+            try {
+                mClientSocket.close();
+            } catch (IOException e) {
+            }
+
+            mBothConnectionsAreAlive = false;
+
+            Logger.log("TCP Forwarding  " + mClientHostPort + " <--> " + mServerHostPort + "  stopped.");
         }
     }
- 
+
 }
